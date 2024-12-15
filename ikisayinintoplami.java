1b7d042b32c8925b4225f086d0cbf232d0d1e6c4
Erdem Karapınar <erdem.karpnar@gmail.com>
2024-12-16 00:11:22 +0300

    Java iki sayının toplamı örneği

---
+++ ikisayinintoplami.java
@@ -0,0 +1,12 @@
+package ikisayinintoplami;
+import java.util.Scanner;
+
+public class ikisayinintoplami {
+	public static void main(String[] args) {
+	int toplam=0;
+		for(int i=1; i<=100; i++) {
+		toplam=toplam+i;
+		System.out.println("1 den 100 e sayýlarýn toplamý:"+toplam);
+	}
+	}
+	}
